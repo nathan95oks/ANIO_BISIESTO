d7130724ec813bd4889fe8b332ad84714f768a38
nathan95oks <nathanaelalba75@gmail.com>
2024-08-24 13:38:36 -0400

    prueba 1 , 2  año 1700 , 2000 bisiesto

--- src/bisiesto.spec.js
+++ src/bisiesto.spec.js
@@ -3,3 +3,7 @@ const esBisiesto = require('./bisiesto');
 test('El año 2000 es bisiesto', () => {
   expect(esBisiesto(2000)).toBe(true);
 });
+
+test('El año 1700 no es bisiesto', () => {
+    expect(esBisiesto(1700)).toBe(false);
+  });
